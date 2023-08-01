ebc8cebd7b344079e61e95889b9df642528b0b68
CynthiaSulo <cynthiasulo2@gmail.com>
2023-08-01 13:03:24 -0400

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square module
+This module illustrates the use of the Square class
+defined in the 1-square module.
+It creates the Square object, prints the square type
+and displays its dictionary representation.
+"""
+
+
+class Square:
+    """This class defines a square using private instance attribute: size."""
+    def __init__(self, size):
+        """This function performs the instantiation of private instance
+        attribute: size with no type or value verification."""
+        self._Square__size = size
