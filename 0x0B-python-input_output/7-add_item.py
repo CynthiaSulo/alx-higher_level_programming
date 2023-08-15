472d103bce7a0f1dac7b940121f5c3f75c14161c
CynthiaSulo <cynthiasulo2@gmail.com>
2023-08-15 18:36:59 -0400

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Imports sys module, save_to_json_file
+and load_from_json file.
+"""
+
+
+from os.path import exists
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    """
+    Adds all arguments to a Python list, and then save them to
+    a file.
+    """
+    filename = "add_item.json"
+    args = sys.argv[1:]
+    args_list = []
+    if exists(filename):
+        args_list = load_from_json_file(filename)
+    else:
+        args_list = []
+    args_list.extend(args)
+    save_to_json_file(args_list, filename
