097c5c5d0382c0c143647ef412fcfc665ee296de
CynthiaSulo <cynthiasulo2@gmail.com>
2023-08-15 18:26:57 -0400

    Task 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module calls read_file function to read my_file_0.txt.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    """
+    with open(filename, 'r') as f:
+        print(f.read(), end="")
